52086707a32da82d1177a5a30af9ecb03db6b3ea
marciommsdc <117790520+marciommsdc@users.noreply.github.com>
2023-12-19 22:18:31 -0300

    Update beerAdd.js

--- beerAdd.js
+++ beerAdd.js
@@ -3,8 +3,7 @@ const cervejasDeposito = ["Heineken", "Stella Artois", "Becks"];
 function adicionaCervejas([...cervejas]) {
     cervejas.push("Colorado", "Estrella Galicia")
     console.log(`Inicialmente a gente tinha apenas essas cervejas no depósito: ${cervejasDeposito}`)
-    console.log(`
-Adicionando com as que chegaram, agora temos: ${cervejas}`)
+    console.log(`Adicionando com as que chegaram, agora temos: ${cervejas}`)
 }
 
 
